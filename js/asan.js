0b17042b8b70e176a4f0a697128f2f320cd5844e
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-10-23 14:23:31 +0900

    15번줄 오타수정

--- js/asan.js
+++ js/asan.js
@@ -11,46 +11,43 @@
   );
 
   // 가정의학과 json 데이터 삽입
+  $('#container').on('click', '.medicalContent .mediList a', function(e){
+    e.preventDefault()
+    var url = this.href; //this.attr('href')
+    var part = this.id; //this.attr('id')
+    $("#container > #content").remove();
+    $("#container").load(url + " #content");
 
-
-// `<li><div class="img"><img src="${photo}" alt=""></div>`
-// `<div class="doctorInfo"><strong>${name}</strong>`
-// `<p>${depart}</p>`
-// `<div>${about}</div></li>`
-
-$('#container').on("click",".medicalContent .mediList a",function(e){
-  e.preventDefult()
-  var url = this.href; //this.attr('href')
-  var part = this.id; //this.attr('id')
-  $("#container > #content").remove();
-  $("#container").load(url + " #content");
-  $.ajax({
-    type:'GET',
-    url:'data/doctors.json',
-    beforeSend: function(xhr){
-      if(xhr.overrideMimeType){
-        xhr.overrideMimeType("application/json");
-      }
-    },
-    success:function(data){
-      var usedata = data[part]
-      var newContent = '';
-      function dataPrint(){
-        for(var i in usedata){
-          newContent += `<li><div class="img"><img src="${usedata[i].photo}" alt=""></div>`
-          newContent += `<div class="doctorInfo"><strong>${usedata[i].name}</strong>`
-          newContent += `<p>${usedata[i].depart}</p>`
-          newContent += `<div>${usedata[i].about}</div></li>`
+    $.ajax({
+      type:'GET',
+      url:'data/doctors.json',
+      beforeSend: function(xhr){
+        if(xhr.overrideMimeType){
+          xhr.overrideMimeType("application/json");
         }
-        $('#content .part1DoctorList').html(`<ul>${newContent}</ul>`)
+      },
+      success:function(data){
+        var usedata = data[part]
+        var newContent = '';
+        function dataPrint(){
+          for(var i in usedata){
+            newContent += `<li><div class="img"><img src="${usedata[i].photo}" alt=""></div>`
+            newContent += `<div class="doctorInfo"><strong>${usedata[i].name}</strong>`
+            newContent += `<p>${usedata[i].depart}</p>`
+            newContent += `<div>${usedata[i].about}</div></div></li>`
+          }
+          $('#content .part1DoctorList').html(`<ul>${newContent}</ul>`)
+        }
+        dataPrint()
+      },
+      error:function(abc){
+        alert(abc.status+'오류발생')
       }
-      dataPrint()
-    },
-    error:function(abc){
-      alert(abc.status+'오류발생')
-    }
+    })
   })
-})
+
+
+
 
 //헤더박스는 스크롤 이벤트시 픽스드됨
  $(window).scroll(function(){
